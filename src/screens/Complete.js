e69fef31e00903974c01430948592f7b0d3e970d
yunhee1 <alkrg007@naver.com>
2023-08-21 15:53:26 +0900

    💄 Feat: Complete page UI

--- src/screens/Complete.js
+++ src/screens/Complete.js
@@ -9,10 +9,44 @@ const Container = styled.SafeAreaView`
   flex: 1;
 `;
 
+const CharacterImg = styled.Image`
+  width: 310px;
+  height: 310px;
+  object-fit: contain;
+`;
+
+const StyledButton = styled.TouchableOpacity`
+  margin-top: 50px;
+  width: 80%;
+  height: 50px;
+  border-radius: 10px;
+  background-color: #ff7360;
+  border: 3px solid #ff6853;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  align-self: center;
+  justify-content: center;
+  margin-top: 15px;
+  font-weight: 800;
+  font-size: 18px;
+`;
+
 const Complete = () => {
   return (
     <Container>
-      <Text>사진 완성 페이지</Text>
+      <Text>우리 쑥쑥이</Text>
+      <Text>사진이 완성되어 도착했어요!</Text>
+      <CharacterImg
+        source={require("../../assets/images/character-complete-img.png")}
+      />
+
+      <Text>자, 이제 우리 쑥쑥이를 만나러 가보실까요?</Text>
+
+      <StyledButton>
+        <ButtonText>우리 아가 만나러 가기</ButtonText>
+      </StyledButton>
     </Container>
   );
 };
