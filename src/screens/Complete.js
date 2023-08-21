385b1415f4cbc0009b74a4f5e4423aecc06e7f16
yunhee1 <alkrg007@naver.com>
2023-08-21 15:26:06 +0900

    ✨ Feat: add complete page

---
+++ src/screens/Complete.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text } from "react-native";
+
+const Complete = () => {
+  return <Text>사진 완성 페이지</Text>;
+};
+
+export default Complete;
